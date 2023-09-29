f4b8fffb8ee876c0d817121feb6f4b1467e169d4
Thatuan6900 <thaituan6900@gmail.com>
2023-09-29 12:07:18 +0700

    pagination ok

--- pet-clinic-frontend/src/app/customer/customer.module.ts
+++ pet-clinic-frontend/src/app/customer/customer.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
 
 import { CustomerComponent } from './customer.component';
 
 @NgModule({
   declarations: [CustomerComponent],
-  imports: [CommonModule, HttpClientModule, TableModule],
+  imports: [CommonModule, HttpClientModule, TableModule, PaginatorModule],
   exports: [CustomerComponent],
 })
 export class CustomerModule {}
